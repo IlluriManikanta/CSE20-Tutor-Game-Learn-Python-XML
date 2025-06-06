424a4ef51732cc11310ce247113459c2404a5ce5
maniilluri <89351612+maniilluri@users.noreply.github.com>
2025-06-05 21:27:06 -0700

    Update AdventureGame.py

--- AdventureGame.py
+++ AdventureGame.py
@@ -1,75 +1,138 @@
 import xml.etree.ElementTree as ET
-import sys
+from time import sleep
+import random
 import os
 
 class AdventureGame:
-    def __init__(self, file_path, player_name, lives):
-        self.prompts = self.load_prompts(file_path)
-        self.player_name = player_name
-        self.score = 0
-        self.lives = lives
-        self.max_lives = lives
+	def __init__(self):
+		self.loadPrompts()
+		self.initialPlayerHealth = 3
+		self.playerHealth = self.initialPlayerHealth
+		self.playerName = "Player 1"
+		self.playerScore = 0
+		self.currentRealm = 1
+		self.currentStage = 1
+		self.numberOfStages = len(self.promptsDict)
+		self.validInputs = ["1", "2"]
 
-    def load_prompts(self, file_path):
-        if not os.path.exists(file_path):
-            print(f"Error: File '{file_path}' not found.")
-            sys.exit(1)
+	def loadPrompts(self):
+		tree = ET.parse('prompts.xml')
+		xmlRoot = tree.getroot()
+		self.promptsDict = {}
+		for stage in xmlRoot:
+			promptList = []
+			outcomePromptList = []
+			for prompt in stage:
+				promptText = prompt.find("promptText").text
+				promptList.append(promptText)
+				option1SuccessPromptText = prompt.find("option1Success").text
+				option2SuccessPromptText = prompt.find("option2Success").text
+				option1FailPromptText = prompt.find("option1Fail").text
+				option2FailPromptText = prompt.find("option2Fail").text
+				outcomePromptList.append((option1SuccessPromptText, option2SuccessPromptText, option2FailPromptText, option1FailPromptText))
+			self.promptsDict[stage.tag] = {"prompts": promptList, "outcomePrompts": outcomePromptList}
+		self.stagePrompts = []
+		self.stageOutcomePrompts = []
+		for key in self.promptsDict:
+			self.stagePrompts.append(self.promptsDict[key]["prompts"])
+			self.stageOutcomePrompts.append(self.promptsDict[key]["outcomePrompts"])
 
-        try:
-            tree = ET.parse(file_path)
-            root = tree.getroot()
-        except ET.ParseError:
-            print("Error: Failed to parse the XML file.")
-            sys.exit(1)
+	def getUserChoice(self):
+		print("\n " + self.playerName + "\033[96m > \033[0m", end="")
+		choice = input("")
+		while choice not in self.validInputs:
+			print("\033[96m >\033[0m Invalid choice, choose 1 or 2")
+			choice = input("\n " + self.playerName + "\033[96m > \033[0m")
+		return int(choice)
 
-        prompts = {}
-        for prompt in root.findall('prompt'):
-            prompt_id = prompt.get('id')
-            text = prompt.find('text').text if prompt.find('text') is not None else ''
-            options = []
-            for option in prompt.findall('option'):
-                option_text = option.text
-                next_prompt = option.get('next')
-                options.append((option_text, next_prompt))
-            prompts[prompt_id] = {'text': text, 'options': options}
-        return prompts
+	def waitUntilUserIsReady(self):
+		print("\033[96m > \033[0m Are you ready to enter the adventure realm, " + self.playerName + "?")
+		choice = input(" " + self.playerName + "\033[96m > \033[0m")
+		while choice.lower() != "yes":
+			print("\n\033[96m > \033[0m Type 'yes' to begin.")
+			choice = input(" " + self.playerName + "\033[96m > \033[0m")
 
-    def loseHealth(self, amount=1):
-        self.lives -= amount
-        print(f"Lives remaining: {self.lives}")
-        if self.lives <= 0:
-            print("You're out of lives. Game over.")
-            sys.exit(0)
+	def outputDeadPlayerPrompt(self):
+		print("\033[96m >\033[91m You have died.\033[0m ")
+		sleep(2)
+		return self.getRestartChoice()
 
-    def play(self):
-        self.outputIntro()
-        current_prompt_id = 'start'
-        while current_prompt_id:
-            if current_prompt_id not in self.prompts:
-                print(f"Error: Prompt ID '{current_prompt_id}' not found.")
-                break
-            prompt = self.prompts[current_prompt_id]
-            print(prompt['text'])
-            options = prompt['options']
-            if not options:
-                print("The adventure ends here.")
-                break
-            for i, (opt_text, _) in enumerate(options, start=1):
-                print(f"{i}. {opt_text}")
-            try:
-                choice = int(input("Choose an option: "))
-                if 1 <= choice <= len(options):
-                    _, next_prompt = options[choice - 1]
-                    self.score += 1
-                    current_prompt_id = next_prompt
-                    if "fail" in next_prompt:
-                        self.loseHealth()
-                else:
-                    print("Invalid choice. Please try again.")
-            except ValueError:
-                print("Invalid input. Please enter a number.")
-        print(f"Thanks for playing, {self.player_name}! Your final score was {self.score}.")
+	def setNumberOfLives(self, numLives):
+		if numLives > self.numberOfStages:
+			numLives = self.numberOfStages
+		self.initialPlayerHealth = numLives
+		self.playerHealth = self.initialPlayerHealth
 
-    def outputIntro(self):
-        print("\033[96mWelcome to the Adventure Game!\033[0m")
-        print(f"Good luck, {self.player_name}. You have {self.lives} lives.")
+	def getPlayerName(self):
+		name = input("\033[96m > \033[0m Enter player name: ")
+		self.playerName = name
+
+	def fetchCurrentStage(self, stageNum):
+		self.currentStage = stageNum + 1
+		promptChoice = random.choice(range(len(self.stagePrompts[stageNum])))
+		prompt = self.stagePrompts[stageNum][promptChoice]
+		winningChoice = random.randint(0, 1)
+		losingChoice = winningChoice + 2
+		winningPrompt = self.stageOutcomePrompts[stageNum][promptChoice][winningChoice]
+		losingPrompt = self.stageOutcomePrompts[stageNum][promptChoice][losingChoice]
+		stageInfo = {
+			"prompt": prompt,
+			"winner": winningChoice + 1,
+			"winningPrompt": winningPrompt,
+			"losingPrompt": losingPrompt
+		}
+		return stageInfo
+
+	def outputResult(self, stageInfo, choice):
+		if stageInfo["winner"] == choice:
+			print("\033[96m > \033[0m" + stageInfo["winningPrompt"])
+			sleep(2)
+			return True
+		else:
+			print("\033[96m > \033[0m" + stageInfo["losingPrompt"])
+			sleep(2)
+			return False
+
+	def getRestartChoice(self):
+		print("\033[96m >\033[0m Would you like to play Again? Enter 'yes' or 'no'.")
+		playAgain = input(" " + self.playerName + "\033[96m >\033[0m ")
+		while playAgain.lower() not in ["yes", "no"]:
+			print("\033[96m >\033[0m Enter 'yes' or 'no'.")
+			playAgain = input(" " + self.playerName + "\033[96m >\033[0m ")
+		return playAgain.lower() == "yes"
+
+	def loseHealth(self, numLives):
+		self.playerHealth -= numLives
+
+	def incrementScore(self):
+		self.playerScore += 1
+		self.currentRealm += 1
+
+	def playerIsDead(self):
+		return self.playerHealth < 1
+
+	def outputPrompt(self, stageInfo):
+		print("\033[96m >\033[0m " + stageInfo["prompt"])
+
+	def outputPlayerStatus(self):
+		print("-------------------------------------------")
+		print("\033[96m > \033[0m Stage " + str(self.currentStage) + "/" + str(self.numberOfStages) +
+		      ", Score: " + str(self.playerScore) + ", Health: " + "\u2764\uFE0F" * self.playerHealth)
+
+	def outputIntro(self):
+		os.system('clear')
+		print("\033[92m-- CSE03's Adventure Realm --\033[0m")
+		print("\033[96m > \033[0m Welcome to our adventure realm! In this world, you will make choices which decide your fate. Make the wrong choice, and it could be your last. Find your way through the realm and you'll earn a point. Lets see how high of a score you can earn... Good luck!\n")
+
+	def outputNewRealmPrompt(self):
+		os.system('clear')
+		print("\033[96m " + self.playerName + " enters the Adventure Realm ...\033[0m")
+
+	def resetPlayerHealth(self):
+		self.playerHealth = self.initialPlayerHealth
+
+	def outputSuccessPrompt(self):
+		print("\033[96m >\033[92m Congratulations, you made it through the adventure realm!\033[0m ")
+		sleep(2)
+		print("\033[96m > \033[0m Your score is now at " + str(self.playerScore) + ". Generating a new realm...")
+		sleep(3)
